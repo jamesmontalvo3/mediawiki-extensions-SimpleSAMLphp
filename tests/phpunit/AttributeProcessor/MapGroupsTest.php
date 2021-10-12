5b11e9def27f3aa7ff9185012db06b873dc22d74
James Montalvo <edwin.j.montalvo@nasa.gov>
2021-10-12 11:23:11 -0500

    Feat: Allow testing SAML attribute values with regex for GroupMap
    Allows specifying matching SAML attributes using regex in addition to
    exact matching. Previously, setting a user to be part of a wiki-group
    based upon multiple SAML attributes could be accomplished with config
    like [ 'wikigroup' => [ 'samlattr' => [ 'grp1', 'grp2', 'grp3' ] ] ];
    With this addition of regex attribute matching, this same can be done
    with [ 'wikigroup' => [ 'samlattr' => [ '/^grp\d+$/' ] ] ];
    
    Change-Id: I48deaf856d7062489c1894f86dbf29a9d3080fb5

--- tests/phpunit/AttributeProcessor/MapGroupsTest.php
+++ tests/phpunit/AttributeProcessor/MapGroupsTest.php
@@ -105,6 +105,45 @@ class MapGroupsTest extends MediaWikiTestCase {
 				[ 'abc' ],
 				[ 'abc', 'editor', 'sysop' ]
 			],
+			'regex-positive' => [
+				[
+					'saml-groups' => [ 'saml-group-1', 'saml-group-2' ]
+				],
+				[
+					'GroupMap' => [ 'wiki_group_from_regex' => [
+						'saml-groups' => [ '/^saml-group-\d+$/' ]
+					] ],
+					'GroupAttributeDelimiter' => null
+				],
+				[ 'sysop' ],
+				[ 'sysop', 'wiki_group_from_regex' ]
+			],
+			'regex-negative' => [
+				[
+					'saml-groups' => [ 'saml-group-a', 'saml-group-b' ]
+				],
+				[
+					'GroupMap' => [ 'wiki_group_from_regex' => [
+						'saml-groups' => [ '/^saml-group-\d+$/' ]
+					] ],
+					'GroupAttributeDelimiter' => null
+				],
+				[ 'sysop' ],
+				[ 'sysop' ]
+			],
+			'regex-remove' => [
+				[
+					'saml-groups' => [ 'saml-group-a', 'saml-group-b' ]
+				],
+				[
+					'GroupMap' => [ 'wiki_group_from_regex' => [
+						'saml-groups' => [ '/^saml-group-\d+$/' ]
+					] ],
+					'GroupAttributeDelimiter' => null
+				],
+				[ 'sysop', 'wiki_group_from_regex' ],
+				[ 'sysop' ]
+			],
 			'delete' => [
 				[
 					// Not in SAML attributes anymore
